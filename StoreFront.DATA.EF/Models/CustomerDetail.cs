e9fb76f5bf759098c7e5a2705d46f26e265ab325
Ben Grochowsky <benwyattg@gmail.com>
2023-12-01 14:37:22 -0600

    implemented Entity Framework

---
+++ StoreFront.DATA.EF/Models/CustomerDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreFront.DATA.EF.Models
+{
+    public partial class CustomerDetail
+    {
+        public CustomerDetail()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public string UserId { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string? State { get; set; }
+        public string? Zip { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
